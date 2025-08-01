280e74a606949bca88c07abaf52bf886eb156b3d
leghart <leghart@wp.pl>
2025-08-01 07:00:01 +0000

    [fix]: init repo

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use cheatess_core::core::engine::{Color, DefaultPrinter, create_board_default};
-use std::io;
-
-fn main() {
-    let board = create_board_default::<DefaultPrinter>(&Color::Black);
-    board.print(&mut io::stdout());
-}
